3e6540cde41f7f358af3d48622b26c12956f8183
bella77-69 <mrspasceri@hotmail.com>
2024-06-15 19:08:36 -0700

    added header and footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
+      <Header />
+  
+    <Footer />
+    </BrowserRouter>
       
     </>
   )
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import './header.scss';
+import logo from '../../assets/logo/logo8.png';
+
+export default function Header() {
+  return (
+    <div className="nav">
+      <Navbar bg="transparent" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              className="nav-logo d-inline-block align-middle mr-2"
+              width="100"
+              alt="logo-img"
+              src={logo}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className='nav-collapse' />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" className="nav-bar">Home</Nav.Link>
+              <Nav.Link href="/search" className="nav-bar">Search</Nav.Link>
+              <Nav.Link href="/contact" className="nav-bar">Contact</Nav.Link>
+              <Nav.Link href="/reviews" className="nav-bar">Reviews</Nav.Link>
+              <NavDropdown title="Wine Generator" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/wines/reds">Red Wine Generator</NavDropdown.Item>
+                <NavDropdown.Item href="/wines/whites">White Wine Generator</NavDropdown.Item>
+                <NavDropdown.Item href="/wines/sparkling">Sparkling Wine Generator</NavDropdown.Item>
+                <NavDropdown.Item href="/wines/port">Port Wine Generator</NavDropdown.Item>
+                <NavDropdown.Item href="/wines/rose">Rose Wine Generator</NavDropdown.Item>
+                <NavDropdown.Item href="/wines/dessert">Dessert Wine Generator</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
