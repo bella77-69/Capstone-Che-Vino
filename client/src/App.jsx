58f4ed988c0375637944b326f144bfd4fc17cd6c
bella77-69 <mrspasceri@hotmail.com>
2024-06-15 19:36:38 -0700

    added Hero, Home page, and  search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 function App() {
 
@@ -10,7 +12,10 @@ function App() {
     <>
       <BrowserRouter>
       <Header />
-  
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/search' element={<Search />} />
+      </Routes>
     <Footer />
     </BrowserRouter>
       
---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./homePage.scss";
+import Hero from "../../components/Hero/Hero";
+
+export default function Home() {
+    return (
+      <section className="main">
+        <Hero />
+       
+      </section>
+    );
+  }
+
+  
